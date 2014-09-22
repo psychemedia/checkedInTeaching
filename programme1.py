75b6dbdf76a878222ae8f38ef151574504840cf6
Tony Hirst <tony.hirst@gmail.com>
2014-09-22 15:54:14 +0100

    A simple scatter plot
    ggplot is based on Leland Wilkinson’s “The Grammar gf Graphics” as
    implemented by Hadley Wickham for the R language.
    
    To generate a chart, we create a new chart object around a dataset, and
    specify a set of aesthetic mappings for it. In this example, we map the
    values in the “carat” column to the x-axis of the chart and the values
    in the “price” column to the to y -axis.
    
    We then specify the sort of geometric mapping we want to apply to the
    chart object - in this case, a simple scatter plot identify as a
    “geom_point() mapping” (that is, each row of data maps onto a point
    geom item using the declared aesthetic mapping.

--- programme1.py
+++ programme1.py
@@ -5,3 +5,7 @@ from ggplot import *
 
 #Preview the diamonds dataset
 diamonds[:3]
+
+#Generate a simple scatter diagram of price against carat weight
+ggplot(diamonds,aes(x="carat",y="price")) + geom_point()
+
